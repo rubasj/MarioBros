24392928bf2f438137e152edc1e73db6abdd7442
rubasj <56114146+rubasj@users.noreply.github.com>
2024-06-07 23:22:29 +0200

    fires and flowers shooting

---
+++ Mario Bros/Code/flower.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+class_name Flower
+
+func _ready():
+	var spawn_tween = get_tree().create_tween()
+	spawn_tween.tween_property(self, "position", position + Vector2(0, -16), .4)
+
+	
